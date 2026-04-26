efa1146a8a40383bcb4f0b0ad0e54776a8b71027
orisabiyi <orisabiyidavid@gmail.com>
2026-04-26 22:32:51 +0100

    style: created a resuable color theme for the brand color and updated logo image

--- src/components/shared/Navbar.vue
+++ src/components/shared/Navbar.vue
@@ -26,20 +26,20 @@ const navLinks = [
 <template>
   <nav class="p-10 2xl:px-32 flex bg-gray-200/20">
     <a href="/" class="flex items-center gap-2 block">
-      <figure class="w-15 h-auto flex items-center justify-center">
+      <figure class="w-20 h-auto flex items-center justify-center">
         <img
           :src="logoImg"
           alt="novashelf logo with no background"
           class="inline-block w-full h-full" />
       </figure>
-      <h1 class="text-2xl font-medium">novashelf</h1>
+      <h1 class="text-2xl text-base-red font-medium">novashelf</h1>
     </a>
 
     <ul class="ml-auto flex items-center gap-10 2xl:text-lg">
       <li
         v-for="navLink in navLinks"
         :key="navLink.path"
-        :class="`${navLink.type === 'button' ? 'bg-[#D85357] px-4 py-1 font-medium rounded-md text-gray-100' : ''}`">
+        :class="`${navLink.type === 'button' ? 'bg-base-red px-4 py-1 font-medium rounded-md text-gray-100' : ''}`">
         <a :href="navLink.path">{{ navLink.name }}</a>
       </li>
     </ul>
